c421ed9f7be4df44d1bbeb80906166d46019f16b
facascante <facascante@smart.com.ph>
2014-11-08 08:52:36 +0800

    temporarily make the table work

--- public/ngscripts/users/controllers.js
+++ public/ngscripts/users/controllers.js
@@ -1,17 +1,6 @@
 angular.module('userApp.controllers',[])
-.controller('UserListController',function($scope,$state,popupService,$window,User, $filter){
+.controller('UserListController',function($scope,$state,popupService,$window,User, $filter, DTOptionsBuilder, DTColumnBuilder){
 
-	var data = [
-                {fullname: "Moroni", username: 50, password: "Moroni", position: 50, email: "Moroni", mobile: 50,status: "Moroni"},
-                {fullname: "Tiancum", username: 43, password: "Moroni", position: 50, email: "Moroni", mobile: 50,status: "Moroni"},
-                {fullname: "Tiancum", username: 43, password: "Moroni", position: 50, email: "Moroni", mobile: 50,status: "Moroni"},
-            ];
-	
-	$scope.users = data;
-		
-	
-//  $scope.users=User.query();
-	
     $scope.deleteUser = function(user){
         if(popupService.showPopup('Really delete this?')){
             user.$delete(function(){
@@ -19,35 +8,7 @@ angular.module('userApp.controllers',[])
             });
         }
     };
-
-}).controller('UserViewController',function($scope,$stateParams,User){
-
-    $scope.user=User.get({id:$stateParams.id});
-
-}).controller('UserCreateController',function($scope,$state,$stateParams,User){
-
-    $scope.user=new User();
-
-    $scope.addUser=function(){
-        $scope.user.$save(function(){
-            $state.go('users');
-        });
-    }
-
-}).controller('UserEditController',function($scope,$state,$stateParams,User){
-
-    $scope.updateUser=function(){
-        $scope.user.$update(function(){
-            $state.go('users');
-        });
-    };
-
-    $scope.loadUser=function(){
-        $scope.user=User.get({id:$stateParams.id});
-    };
-
-    $scope.loadUser();
-}).controller('withBootstrapOptionsCtrl', function ($scope, DTOptionsBuilder, DTColumnBuilder) {
+    
     $scope.dtOptions = DTOptionsBuilder
     .fromSource('/api/users')
     // Add Bootstrap compatibility
@@ -79,26 +40,54 @@ angular.module('userApp.controllers',[])
         'pdf',
         'xls',
     ]);
-$scope.dtColumns = [
-    DTColumnBuilder.newColumn('fullname').withTitle('Fullname'),
-    DTColumnBuilder.newColumn('username').withTitle('Username'),
-    DTColumnBuilder.newColumn('position').withTitle('Position'),
-    DTColumnBuilder.newColumn('phone').withTitle('Phone'),
-    DTColumnBuilder.newColumn('email').withTitle('Email'),
-    DTColumnBuilder.newColumn('status').withTitle('Status'),
-    DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable()
-    .renderWith(function(data, type, full, meta) {
-        return '<a href="#/users/view", ui-sref="editUser" class="tooltips btn default" '+
-        	'data-container="body", data-placement="top", '+
-        	'data-html="true", data-original-title="View Record">' +
-            '   <i class="fa fa-eye"></i>' +
-            '</a>&nbsp;' +
-            
-            '<a href="#/users/edit", ui-sref="editUser" class="tooltips btn default" '+
-        	'data-container="body", data-placement="top", '+
-        	'data-html="true", data-original-title="Edit Record">' +
-            '   <i class="fa fa-edit"></i>' +
-            '</a>&nbsp;';
-    })
-];
+	$scope.dtColumns = [
+	    DTColumnBuilder.newColumn('fullname').withTitle('Fullname'),
+	    DTColumnBuilder.newColumn('username').withTitle('Username'),
+	    DTColumnBuilder.newColumn('position').withTitle('Position'),
+	    DTColumnBuilder.newColumn('phone').withTitle('Phone'),
+	    DTColumnBuilder.newColumn('email').withTitle('Email'),
+	    DTColumnBuilder.newColumn('status').withTitle('Status'),
+	    DTColumnBuilder.newColumn(null).withTitle('Actions').notSortable()
+	    .renderWith(function(data, type, full, meta) {
+	        return '<a href="#/users/view", ui-sref="editUser" class="tooltips btn default" '+
+	        	'data-container="body", data-placement="top", '+
+	        	'data-html="true", data-original-title="View Record">' +
+	            '   <i class="fa fa-eye"></i>' +
+	            '</a>&nbsp;' +
+	            
+	            '<a href="#/users/edit", ui-sref="editUser" class="tooltips btn default" '+
+	        	'data-container="body", data-placement="top", '+
+	        	'data-html="true", data-original-title="Edit Record">' +
+	            '   <i class="fa fa-edit"></i>' +
+	            '</a>&nbsp;';
+	    })
+	];
+
+}).controller('UserViewController',function($scope,$stateParams,User){
+
+    $scope.user=User.get({id:$stateParams.id});
+
+}).controller('UserCreateController',function($scope,$state,$stateParams,User){
+
+    $scope.user=new User();
+
+    $scope.addUser=function(){
+        $scope.user.$save(function(){
+            $state.go('users');
+        });
+    }
+
+}).controller('UserEditController',function($scope,$state,$stateParams,User){
+
+    $scope.updateUser=function(){
+        $scope.user.$update(function(){
+            $state.go('users');
+        });
+    };
+
+    $scope.loadUser=function(){
+        $scope.user=User.get({id:$stateParams.id});
+    };
+
+    $scope.loadUser();
 });
