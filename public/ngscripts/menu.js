eff0ceb69b219ecbf8abd771efe6f3737b020e3b
facascante <facascante@smart.com.ph>
2014-11-13 07:25:07 +0800

    fix menu

--- public/ngscripts/menu.js
+++ public/ngscripts/menu.js
@@ -17,107 +17,130 @@
 		   link : "javascript:;",
 		   title : "Sales",
 		   start : true,
-		   icon : "fa-home",
+		   icon : "fa-shopping-cart",
 		   id : 1,
 		   childLink : [
-		      {
-		          title : "Sales Order",
-		          description : "Create Sales Order",
-							sref : "salesOrder"
-		      },
+			      {
+			          title : "Sales Order",
+			          description : "Create Sales Order",
+								sref : "salesOrder"
+			      },
+			      {
+			          title : "Return Merchandise",
+			          description : "Return Sales Item",
+								sref : "salesReturn"
+			      }
+			   ]
+		   },
+			 {
+				link : "javascript:;",
+				title : "Warehouse",
+				last : true,
+				icon : "fa-archive",
+				id : 2,
+				childLink : [
 					{
 							title : "Print Pick List",
 							description : "Prepare Items for Delivery",
 							sref : "salesPickList"
 					},
-		      {
-		          title : "Delivery Receipts",
-		          description : "Prepare and Approve Items for Delivery",
-							sref : "salesDelivery"
-		      },
-		      {
-		          title : "Sales Invoices",
-		          description : "Approve Sales Invoices",
-							sref : "salesInvoice"
-		      },
 					{
-							title : "Print DR and SI",
-							description : "Print Dr and SI",
-							sref : "salesPrint"
+							title : "Delivery Receipts",
+							description : "Prepare and Approve Items for Delivery",
+							sref : "salesDelivery"
 					},
 					{
 							title : "Trip Ticket",
 							description : "Print and Update Trip Ticket",
 							sref : "salesTripTicket"
 					},
-		      {
-		          title : "Return Merchandise",
-		          description : "Return Sales Item",
-							sref : "salesReturn"
-		      },
-		      {
-		          title : "Credit Memo",
-		          description : "Apply for Credit Memo",
-							sref : "salesMemo"
-		      },
 					{
-							title : "Payment",
-							description : "Record Payment",
-							sref : "salesPayment"
-					}
-		   ]
-	   },
-	   {
-		   link : "javascript:;",
-		   title : "Admin",
-		   last : true,
-		   icon : "fa-gear",
-		   id : 2,
-		   childLink : [
-		      {
-		          title : "Customer",
-		          description : "Manage Customers",
-							sref : "customers"
-		      },
-		      {
-		          title : "Products",
-		          description : "Manage Products",
-							sref : "products"
-		      },
-		      {
-		          title : "Users",
-		          description : "Manage Users",
-							sref : "users"
-		      }
-		   ]
-	   },
-		{
-			link : "javascript:;",
-			title : "Shipment",
-			last : true,
-			icon : "fa-gear",
-			id : 2,
-			childLink : [
+							title : "Print DR and SI",
+							description : "Print Dr and SI",
+							sref : "salesPrint"
+					},
+					{
+							title : "Return Merchandise",
+							description : "Return Sales Item",
+							sref : "salesReturn"
+					},
+				]
+			},
+			{
+				link : "javascript:;",
+				title : "Finance",
+				start : true,
+				icon : "fa-suitcase",
+				id : 1,
+				childLink : [
+						{
+								title : "Sales Invoices",
+								description : "Approve Sales Invoices",
+								sref : "salesInvoice"
+						},
+						{
+								title : "Credit Memo",
+								description : "Apply for Credit Memo",
+								sref : "salesMemo"
+						},
+						{
+								title : "Payment",
+								description : "Record Payment",
+								sref : "salesPayment"
+						}
+					]
+				},
 				{
-						title : "Shipments",
-						description : "Manage Shipments",
-						sref : "shipments"
-				}
-			]
-		},
-		{
-			link : "javascript:;",
-			title : "Purchase",
-			last : true,
-			icon : "fa-gear",
-			id : 2,
-			childLink : [
+					link : "javascript:;",
+					title : "Shipment",
+					last : true,
+					icon : "fa-truck",
+					id : 2,
+					childLink : [
+						{
+								title : "Shipments",
+								description : "Manage Shipments",
+								sref : "shipments"
+						}
+					]
+				},
 				{
-						title : "Purchase",
-						description : "Manage Purchase",
-						sref : "purchases"
-				}
-			]
-		}
+					link : "javascript:;",
+					title : "Purchase",
+					last : true,
+					icon : "fa-plane",
+					id : 2,
+					childLink : [
+						{
+								title : "Purchase",
+								description : "Manage Purchase",
+								sref : "purchases"
+						}
+					]
+				},
+			   {
+				   link : "javascript:;",
+				   title : "Admin",
+				   last : true,
+				   icon : "fa-gear",
+				   id : 2,
+				   childLink : [
+				      {
+				          title : "Customer",
+				          description : "Manage Customers",
+									sref : "customers"
+				      },
+				      {
+				          title : "Products",
+				          description : "Manage Products",
+									sref : "products"
+				      },
+				      {
+				          title : "Users",
+				          description : "Manage Users",
+									sref : "users"
+				      }
+				   ]
+			   },
 	];
 })();
